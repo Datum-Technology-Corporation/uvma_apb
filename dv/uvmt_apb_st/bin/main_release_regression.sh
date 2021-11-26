15db3e2c0293ef3b0b9088899c61d2698786fee8
dpoulin <dpoulin@localhost>
2021-11-25 19:20:52 -0500

    adding functional coverage report generation to regressions

--- dv/uvmt_apb_st/bin/main_release_regression.sh
+++ dv/uvmt_apb_st/bin/main_release_regression.sh
@@ -19,4 +19,4 @@ source ./setup_terminal.sh
 ../tools/.imports/mio/src/mio.py sim uvmt_apb_st -t writes -s 1 -c
 ../tools/.imports/mio/src/mio.py sim uvmt_apb_st -t all_access -s 1 -c
 ../tools/.imports/mio/src/mio.py results uvmt_apb_st results
-#../tools/.imports/mio/src/mio.py cov uvmt_apb_st
+../tools/.imports/mio/src/mio.py cov uvmt_apb_st
