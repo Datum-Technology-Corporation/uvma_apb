4f593a63c1dfd28313932c60f8fe8ddf42e46fc4
datum-dpoulin <dpoulin@datumtc.ca>
2022-06-21 20:24:40 -0400

    Fixing regressions

--- dv/uvmt_apb_st/bin/main_release_regression.sh
+++ dv/uvmt_apb_st/bin/main_release_regression.sh
@@ -12,11 +12,11 @@
 
 
 # Launched from uvml project sim dir
-./setup_project.py
+python ./setup_project.py
 source ./setup_terminal.sh
-../tools/.imports/mio/src/mio.py cpel uvmt_apb_st
-../tools/.imports/mio/src/mio.py sim uvmt_apb_st -t reads -s 1 -c
-../tools/.imports/mio/src/mio.py sim uvmt_apb_st -t writes -s 1 -c
-../tools/.imports/mio/src/mio.py sim uvmt_apb_st -t all_access -s 1 -c
-../tools/.imports/mio/src/mio.py results uvmt_apb_st results
-../tools/.imports/mio/src/mio.py cov uvmt_apb_st
+../tools/.imports/mio/src/__main__.py cpel uvmt_apb_st
+../tools/.imports/mio/src/__main__.py sim uvmt_apb_st -t reads -s 1 -c
+../tools/.imports/mio/src/__main__.py sim uvmt_apb_st -t writes -s 1 -c
+../tools/.imports/mio/src/__main__.py sim uvmt_apb_st -t all_access -s 1 -c
+../tools/.imports/mio/src/__main__.py results uvmt_apb_st results
+../tools/.imports/mio/src/__main__.py cov uvmt_apb_st
